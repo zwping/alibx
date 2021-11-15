fe42de1b7c4f117abf0fe4b255b50508e02b0f24
zwping <1101558280@qq.com>
2021-11-15 13:00:23 +0800

    auto sync code

--- app/src/main/java/com/zwping/a/Test.java
+++ app/src/main/java/com/zwping/a/Test.java
@@ -1,14 +1,26 @@
 package com.zwping.a;
 
+import androidx.annotation.Nullable;
+
 import com.google.android.material.button.MaterialButton;
+import com.zwping.alibx.IJson;
 import com.zwping.alibx.ItemViewType;
 import com.zwping.alibx.ViewKtx;
 import com.zwping.alibx.ViewKtxKt;
+
+import org.json.JSONObject;
 
 /**
  * zwping @ 2021/11/10
  */
 public class Test {
 
+    public static class Bean extends IJson{
+        public String title;
+
+        public Bean(@Nullable JSONObject obj) {
+            super(obj, true);
+        }
+    }
 
 }
