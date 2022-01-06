b4ea4ac37f0abaec1300216195e309b88bcaaf67
zwping <1101558280@qq.com>
2022-01-06 13:38:04 +0800

    1

---
+++ demo/src/main/java/com/zwping/alibx/demo/AcMain.kt
@@ -0,0 +1,11 @@
+package com.zwping.alibx.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac_main)
+    }
+}
